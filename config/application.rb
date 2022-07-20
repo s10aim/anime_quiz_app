d6cb7d8c4e23d9235a191f1a12528b9d1b8e324f
tanahara <s10.vermouth.4869@gmail.com>
2022-07-20 17:03:09 +0900

    rails g コマンドで生成されるファイルの設定を追加

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,11 @@ module AnimeQuizApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.assets false
+      g.skip_routes false
+      g.helper false
+      g.system_tests = nil
+    end
   end
 end
