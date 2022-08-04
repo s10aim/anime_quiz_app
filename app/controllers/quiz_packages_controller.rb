320a52dcbcb9114e5f60182fe4584a5b974b71e5
tanahara <s10.vermouth.4869@gmail.com>
2022-08-04 19:57:39 +0900

    クイズに回答したらまた次のクイズが表示されるよう実装

--- app/controllers/quiz_packages_controller.rb
+++ app/controllers/quiz_packages_controller.rb
@@ -2,6 +2,8 @@ class QuizPackagesController < ApplicationController
   def update
     quiz_package = QuizPackage.find(params[:id])
     quiz_package.update(quiz_package_params)
+    next_quiz_position = quiz_package.position + 1
+    @next_quiz_package = quiz_package.package.quiz_packages.find_by(position: next_quiz_position)
   end
 
   private
