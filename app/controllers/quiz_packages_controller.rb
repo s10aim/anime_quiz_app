9b5cdb7f069adea6e63beb86289dbb870b440c55
tanahara <s10.vermouth.4869@gmail.com>
2022-08-05 19:08:50 +0900

    最後のクイズに回答したら finished_at を更新

--- app/controllers/quiz_packages_controller.rb
+++ app/controllers/quiz_packages_controller.rb
@@ -1,13 +1,23 @@
 class QuizPackagesController < ApplicationController
+  before_action :set_quiz_package
+
   def update
-    quiz_package = QuizPackage.find(params[:id])
-    quiz_package.update(quiz_package_params)
-    next_quiz_position = quiz_package.position + 1
-    @next_quiz_package = quiz_package.package.quiz_packages.find_by(position: next_quiz_position)
-    redirect_to root_path if @next_quiz_package.nil?
+    @quiz_package.update(quiz_package_params)
+    next_quiz_position = @quiz_package.position + 1
+    @next_quiz_package = @quiz_package.package.quiz_packages.find_by(position: next_quiz_position)
+    last_quiz_package if @next_quiz_package.nil?
   end
 
   private
+
+  def last_quiz_package
+    @quiz_package.package.update(finished_at: Time.zone.now)
+    redirect_to root_path
+  end
+
+  def set_quiz_package
+    @quiz_package = QuizPackage.find(params[:id])
+  end
 
   def quiz_package_params
     params.require(:quiz_package).permit(:choice_id)
