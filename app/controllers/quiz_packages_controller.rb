22afd4db6e5d73dc683661e39618052d9ffc3765
tanahara <s10.vermouth.4869@gmail.com>
2022-08-05 15:16:03 +0900

    最後のクイズに回答したら別のページにリダイレクトするように実装

--- app/controllers/quiz_packages_controller.rb
+++ app/controllers/quiz_packages_controller.rb
@@ -4,6 +4,7 @@ class QuizPackagesController < ApplicationController
     quiz_package.update(quiz_package_params)
     next_quiz_position = quiz_package.position + 1
     @next_quiz_package = quiz_package.package.quiz_packages.find_by(position: next_quiz_position)
+    redirect_to root_path if @next_quiz_package.nil?
   end
 
   private
