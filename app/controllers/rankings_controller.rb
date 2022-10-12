634dd63561b4689130bf3f6185d6eb3f63cf04f4
tanahara <s10.vermouth.4869@gmail.com>
2022-10-12 20:52:20 +0900

    ランキングトップ画面のスタイルを調整

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,2 +1,6 @@
 class RankingsController < ApplicationController
+  def show
+    @my_best_score_of_complete = Package.my_best_score_of(current_user, 'complete')
+    @my_best_score_of_selected = Package.my_best_score_of(current_user, 'selected')
+  end
 end
