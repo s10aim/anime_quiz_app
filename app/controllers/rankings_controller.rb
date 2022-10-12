3e1e0940fefd5fce6a14f51f2776ed40302e9b7b
tanahara <s10.vermouth.4869@gmail.com>
2022-10-13 01:05:54 +0900

    ゲストの場合のランキング表示を修正

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,5 +1,7 @@
 class RankingsController < ApplicationController
   def show
+    return if current_user.nil?
+
     @my_best_score_of_complete = Package.my_best_score_of(current_user, 'complete')
     @my_best_score_of_selected = Package.my_best_score_of(current_user, 'selected')
   end
